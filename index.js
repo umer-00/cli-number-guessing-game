05eb8198d7f800b2a0faa48b7e69b8bc03053dc4
umer-00 <uf4673515@gmail.com>
2024-03-27 03:36:20 +0500

    adding a number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tNumber-Quest-cli-number-guessing-game\n\t");
+const randomNumber = Math.floor(Math.random() * 9 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Pick a number from 1-10 :  ",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("congratulations ! you guessed arighr number");
+}
+else {
+    console.log("You guessed a wrong number try again ...");
+}
